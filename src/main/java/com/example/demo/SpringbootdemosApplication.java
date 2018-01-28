8e35024e81edd5d808a74cf84422c079d6b34816
liuyumeng <liuyumeng@dxyer.com>
2018-01-28 22:57:37 +0800

    change

---
+++ src/main/java/com/example/demo/SpringbootdemosApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootdemosApplication {
+
+	public static void main(String[] args) {
+		System.out.println("正在启动。。。");
+		SpringApplication.run(SpringbootdemosApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/demo/threadsafety/sync/Foo.java
@@ -0,0 +1,7 @@
+package com.example.demo.threadsafety.sync;
+
+/**
+ * Created by liuyumeng on 2018/1/20.
+ */
+public class Foo {
+}
