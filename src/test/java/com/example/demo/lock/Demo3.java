edf0c6c99fb29e1afbc0f4eba343e0d44936cf1b
liuyumeng <liuyumeng@dxyer.com>
2018-01-28 17:18:27 +0800

    move-lock demos

---
+++ src/test/java/com/example/demo/lock/Demo3.java
@@ -0,0 +1,27 @@
+package com.example.demo.lock;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Created by liuyumeng on 2018/1/20.
+ */
+public class Demo3 implements Runnable {
+    static AtomicInteger race = new AtomicInteger(0);
+
+    @Override
+    public void run() {
+        for (int j = 0; j < 1000000; j++) {
+            race.incrementAndGet();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(new Demo3());
+        Thread t2 = new Thread(new Demo3());
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        System.out.println(race.get());
+    }
+}
---
+++ src/test/java/com/example/demo/lock/Foo.java
@@ -0,0 +1,7 @@
+package com.example.demo.lock;
+
+/**
+ * Created by liuyumeng on 2018/1/20.
+ */
+public class Foo {
+}
