0a71955f10673de8a7fcd5a67b07cedaa8a1d073
liuyumeng <liuyumeng@dxyer.com>
2018-02-09 17:15:39 +0800

    spring boot-profile

--- src/main/java/com/example/demo/SpringbootdemosApplication.java
+++ src/main/java/com/example/demo/SpringbootdemosApplication.java
@@ -6,15 +6,15 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 
 /**
  * 可以关闭特定的配置 例:
- * @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
  *
+ * @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
  */
 @SpringBootApplication
 public class SpringbootdemosApplication {
 
-	public static void main(String[] args) {
-		System.out.println("正在启动。。。");
-		new SpringApplicationBuilder(SpringbootdemosApplication.class)
-				.run();
-	}
+    public static void main(String[] args) {
+        System.out.println("正在启动。。。");
+        new SpringApplicationBuilder(SpringbootdemosApplication.class)
+                .run(args);
+    }
 }
