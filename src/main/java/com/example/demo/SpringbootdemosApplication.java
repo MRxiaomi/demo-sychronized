8207ad7c0d987375c9c3b6ce0a0b289dde6f902c
liuyumeng <liuyumeng@dxyer.com>
2018-02-09 16:23:31 +0800

    spring boot-banner http://patorjk.com/software/taag

--- src/main/java/com/example/demo/SpringbootdemosApplication.java
+++ src/main/java/com/example/demo/SpringbootdemosApplication.java
@@ -1,13 +1,20 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
+
+/**
+ * 可以关闭特定的配置 例:
+ * @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+ *
+ */
 @SpringBootApplication
 public class SpringbootdemosApplication {
 
 	public static void main(String[] args) {
 		System.out.println("正在启动。。。");
-		SpringApplication.run(SpringbootdemosApplication.class, args);
+		new SpringApplicationBuilder(SpringbootdemosApplication.class)
+				.run();
 	}
 }
