2bc06892ef4e4b690d6e0626c2d1fb7c176fb24d
liuyumeng <liuyumeng@dxyer.com>
2018-09-21 11:19:47 +0800

    change - bio 单线程模型

---
+++ src/test/java/com/example/demo/nio/reactor/bio/BioServer.java
@@ -0,0 +1,57 @@
+package com.example.demo.nio.reactor.bio;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.nio.charset.Charset;
+
+/**
+ * @author liuyumeng
+ *         <p>
+ *         单线程模型
+ *         <p>
+ *         仅有一个线程
+ *         <p>
+ *         只能处理一个请求
+ *         <p>
+ *         无法利用多核优势
+ */
+public class BioServer implements Runnable {
+    private int port;
+
+    private ServerSocket serverSocket;
+
+    public BioServer(int port) {
+        this.port = port;
+        try {
+            serverSocket = new ServerSocket();
+            serverSocket.bind(new InetSocketAddress(port));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //
+    public void run() {
+        try {
+            while (true) {
+                //连接操作
+                java.net.Socket socket = serverSocket.accept();
+
+                //read操作
+                InputStream inputStream = socket.getInputStream();
+
+                System.out.println("输入...");
+                //write操作
+                OutputStream outputStream = socket.getOutputStream();
+                System.out.println("输出...");
+
+                outputStream.write("abc".getBytes(Charset.forName("UTF-8")));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
