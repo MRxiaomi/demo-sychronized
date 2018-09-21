eaaa4eb07d0541a77ac6a9c75e68568f2e6c0f27
liuyumeng <liuyumeng@dxyer.com>
2018-09-21 17:37:59 +0800

    change - nio 多reactor多线程模型

---
+++ src/test/java/com/example/demo/nio/reactor/multireactor/Reactor.java
@@ -0,0 +1,15 @@
+package com.example.demo.nio.reactor.multireactor;
+
+/**
+ * @author liuyumeng
+ *
+ * 多reactor 多 work 线程
+ *
+ * 作用：
+ *  1.解决1个服务端监听线程无法有效处理所有客户端连接的性能不足
+ *  2.每个模块的工作更加专一，耦合度更低
+ *
+ */
+public class Reactor {
+
+}
